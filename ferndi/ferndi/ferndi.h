bdc402de70c70afcad5b763436f661531897eb71
Virux <starletgh@gmail.com>
2023-06-20 23:41:26 +0000

    build.grandle set up for android

---
+++ ferndi/ferndi/ferndi.h
@@ -0,0 +1,18 @@
+//
+//  ferndi.h
+//  ferndi
+//
+//  Created by Rasheed Starlet on 20/06/2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ferndi.
+FOUNDATION_EXPORT double ferndiVersionNumber;
+
+//! Project version string for ferndi.
+FOUNDATION_EXPORT const unsigned char ferndiVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ferndi/PublicHeader.h>
+
+
