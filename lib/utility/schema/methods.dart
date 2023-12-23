dd49208f21c564af246615c95b28bac8cdad96fe
rasheedmhd <starletgh@gmail.com>
2023-12-23 11:44:53 +0000

    added utility folder for code refactoring

---
+++ lib/utility/schema/methods.dart
@@ -0,0 +1,80 @@
+import 'package:app/models/schemas.dart';
+
+final wallets = realm.all<Wallet>();
+final spends = realm.all<Spend>();
+final categories = realm.all<Category>();
+final duration = realm.all<Duration>();
+final subscriptions = realm.all<Subscription>();
+// for later feature
+// final budgets = realm.all<Budget>;
+
+// Querying data for balance card
+String balance = wallets.map((wallet) => wallet.balance).toList().first;
+final income = realm.query<Wallet>('name == \$0', ['Income']).first;
+final totalSpend = spends.map((spend) => spend.amount).toList().first;
+final totalTransactions = spends.map((spend) => spend.amount).toList().length;
+
+// Create a new wallet and persist to db
+void createWallet(Wallet wallet) {
+  realm.write(() {
+    realm.add(wallet);
+  });
+}
+
+void deleteWallet(Wallet wallet) {
+  realm.write(() {
+    realm.delete(wallet);
+  });
+}
+
+// Create a new spend record and persist to db
+void recordSpend(Spend spend) {
+  realm.write(() {
+    realm.add(spend);
+  });
+}
+
+void deleteSpend(Spend spend) {
+  realm.write(() {
+    realm.delete<Spend>(spend);
+  });
+}
+
+// Create a new spend record and persist to db
+void addSubscription(Subscription subscription) {
+  realm.write(() {
+    realm.add(subscription);
+  });
+}
+
+void deleteSubscription(Subscription subscription) {
+  realm.write(() {
+    realm.delete(subscription);
+  });
+}
+
+// Create a new Category record and persist to db
+void createCategory(Category category) {
+  realm.write(() {
+    realm.add(category);
+  });
+}
+
+// Create a bunch of Categories when getting onboard
+void addCategories() {
+  realm.write(() {
+    realm.addAll(Categories);
+  });
+}
+
+void addDurations() {
+  realm.write(() {
+    realm.addAll(Durations);
+  });
+}
+
+void deleteCategory(Category category) {
+  realm.write(() {
+    realm.delete(category);
+  });
+}
