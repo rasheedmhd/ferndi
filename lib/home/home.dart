4af164ba663a3bd263e4406299643944d51e48d0
rasheedmhd <starletgh@gmail.com>
2024-01-03 20:50:18 +0000

    displaying transactions count

--- lib/home/home.dart
+++ lib/home/home.dart
@@ -1,4 +1,5 @@
 import "package:app/home/spends.dart";
+import "package:app/utility/schema/methods.dart";
 import "package:flutter/material.dart";
 import "package:app/cards/balance.dart";
 import "package:app/cards/addSpend.dart";
@@ -12,76 +13,82 @@ class Home extends StatefulWidget {
 }
 
 class HomeState extends State<Home> {
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "Home",
-      theme: ThemeData(fontFamily: 'Gilroy'),
-      home: Scaffold(
-        appBar: AppBar(
-          title: const 
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
+        title: "Home",
+        theme: ThemeData(fontFamily: 'Gilroy'),
+        home: Scaffold(
+            appBar: AppBar(
+              title: const Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Row(
+                    children: [
+                      FaIcon(FontAwesomeIcons.chevronLeft,
+                          size: 20, color: Colors.white),
+                      SizedBox(
+                        width: 12,
+                      ),
+                      Text(
+                        "This Month",
+                        style: TextStyle(color: Colors.white),
+                      ),
+                      SizedBox(
+                        width: 12,
+                      ),
+                      FaIcon(FontAwesomeIcons.chevronRight,
+                          size: 20, color: Colors.white),
+                    ],
+                  ),
+                ],
+              ),
+              backgroundColor: const Color.fromARGB(255, 5, 61, 135),
+            ),
+            body: ListView(
+              padding: const EdgeInsets.all(15),
               children: [
-
+                const BalanceCard(),
+                const SizedBox(
+                  height: 10,
+                ),
+                const AddSpendCard(),
+                const SizedBox(
+                  height: 20,
+                ),
                 Row(
                   children: [
-
-                    FaIcon(
-                      FontAwesomeIcons.chevronLeft, 
-                      size: 20,
-                      color: Colors.white
-                    ),
-                    SizedBox( width: 12,),
-                    Text(
-                      "This Month",
+                    const Text(
+                      "Spend History",
                       style: TextStyle(
-                        color: Colors.white
+                        fontSize: 30.0,
+                        color: Color.fromARGB(255, 5, 61, 135),
+                        fontWeight: FontWeight.w700,
                       ),
                     ),
-                    SizedBox( width: 12,),
-                    FaIcon(
-                      FontAwesomeIcons.chevronRight,
-                      size: 20,
-                      color: Colors.white
+                    const Spacer(),
+                    const FaIcon(FontAwesomeIcons.arrowRightLong,
+                        size: 23, color: Color.fromARGB(255, 151, 151, 151)),
+                    Text(
+                      " $totalTransactions ",
+                      style: const TextStyle(
+                        fontSize: 30.0,
+                        color: Color.fromARGB(255, 151, 151, 151),
+                        fontWeight: FontWeight.w700,
+                      ),
                     ),
                   ],
                 ),
+                const SizedBox(
+                  height: 10,
+                ),
+                const SizedBox(
+                  //padding: EdgeInsets.fromLTRB(15, 10, 0, 0),
+                  height: 650,
+                  // height: double.infinity,
+                  child: Spends(),
+                ),
               ],
-            ),
-          backgroundColor: const Color.fromARGB(255, 5, 61, 135),
-        ),
-        body: ListView(
-          padding: const EdgeInsets.all(15),
-          children: const [
-            BalanceCard(),
-            SizedBox(
-              height: 10,
-            ),
-            AddSpendCard(),
-            SizedBox(
-              height: 20,
-            ),
-            Text(
-              "Spend History",
-              style: TextStyle(
-                fontSize: 30.0,
-                color: Color.fromARGB(255, 5, 61, 135),
-                fontWeight: FontWeight.w700,
-                // fontFamily: "WorkSans"
-              ),
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            SizedBox(
-              //padding: EdgeInsets.fromLTRB(15, 10, 0, 0),
-              height: 650,
-              // height: double.infinity,
-              child: Spends(),
-            ),
-          ],
-    )));
+            )));
   }
 }
