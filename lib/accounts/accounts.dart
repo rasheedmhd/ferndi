855175c9382508bb550bf2f484e651b1b5d0b236
rasheedmhd <starletgh@gmail.com>
2024-01-04 16:51:35 +0000

    redesigned accounts page

--- lib/accounts/accounts.dart
+++ lib/accounts/accounts.dart
@@ -38,14 +38,19 @@ class AccountsState extends State<Accounts> {
         home: Scaffold(
           appBar: AppBar(
             actions: [
-              IconButton(onPressed: _addCategory, icon: const Icon(Icons.add, color: Colors.white,))
+              IconButton(onPressed: _addCategory, icon: const Icon(
+                Icons.add, 
+                size: 34,
+                color: Color.fromARGB(255, 19, 194, 110),
+                )
+              )
             ],
             title: const Text(
               "Accounts", 
               style: TextStyle(
-                color: Colors.white
+                color: Color.fromARGB(255, 19, 194, 110),
             ),),
-            backgroundColor: const Color.fromARGB(255, 5, 61, 135),
+            backgroundColor: const Color.fromARGB(255, 241, 255, 248),
           ),
           floatingActionButton: FloatingActionButton(
             shape: RoundedRectangleBorder(
@@ -53,32 +58,34 @@ class AccountsState extends State<Accounts> {
             ),
             backgroundColor: const Color.fromARGB(255, 5, 61, 135),
             onPressed: _addWallet,
-            child: const Icon(Icons.add, color: Colors.white),
+            child: const Icon(
+              Icons.add, color: Colors.white),
             ),
-          body: ListView(
-            padding: const EdgeInsets.all(15),
-            children: const [
-              Text(
-                "Wallets",
-                style: TextStyle(
-                  fontSize: 30.0,
-                  color: Color.fromARGB(255, 5, 61, 135),
-                  fontWeight: FontWeight.w700,
+          body: Container(
+            color: const Color.fromARGB(255, 241, 255, 248),
+            child: ListView(
+              padding: const EdgeInsets.all(15),
+              children: const [
+                Text(
+                  "Your Wallets",
+                  style: TextStyle(
+                    fontSize: 30.0,
+                    color: Color.fromARGB(255, 48, 136, 6),
+                    fontWeight: FontWeight.w700,
+                  ),
                 ),
-              ),
-
-              SizedBox(
-                height: 12,
-              ),
-
-              Divider(), 
-
-              SizedBox(
-                height: 2800,
-                child: WalletsCard(),
-              )
-            ],
+            
+                SizedBox(
+                  height: 12,
+                ),
+                        
+                SizedBox(
+                  height: 2800,
+                  child: WalletsCard(),
+                )
+              ],
+            ),
           ),
         ));
   }
-}
+}  
