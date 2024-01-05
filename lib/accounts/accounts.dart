929ed43cdda81eb1917dbba4df9a0f26bb2b8e78
rasheedmhd <starletgh@gmail.com>
2024-01-05 19:50:50 +0000

    querying data from selected wallets to display in wallets card section

--- lib/accounts/accounts.dart
+++ lib/accounts/accounts.dart
@@ -36,101 +36,101 @@ class AccountsState extends State<Accounts> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: "Accounts",
-        theme: ThemeData(fontFamily: 'Gilroy'),
-        home: Scaffold(
-            appBar: AppBar(
-              actions: [
-                IconButton(
-                  onPressed: _addCategory,
-                  icon: const Icon(
-                    Icons.add,
-                    size: 34,
-                    color: Color.fromARGB(255, 255, 255, 255),
-                  ))
-              ],
-              title: const Text(
-                "Accounts",
-                style: TextStyle(
-                  color: Color.fromARGB(255, 255, 255, 255),
-                ),
-              ),
+      title: "Accounts",
+      theme: ThemeData(fontFamily: 'Gilroy'),
+      home: Scaffold(
+        appBar: AppBar(
+          actions: [
+            IconButton(
+              onPressed: _addCategory,
+              icon: const Icon(
+                Icons.add,
+                size: 34,
+                color: Color.fromARGB(255, 255, 255, 255),
+              ))
+          ],
+          title: const Text(
+            "Accounts",
+            style: TextStyle(
+              color: Color.fromARGB(255, 255, 255, 255),
+            ),
+          ),
+          backgroundColor: const Color.fromARGB(255, 19, 194, 110),
+        ),
+        floatingActionButton: AvatarGlow(
+          glowColor: const Color.fromARGB(255, 5, 61, 135),
+          child: FloatingActionButton(
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(50.0)),
+            backgroundColor: const Color.fromARGB(255, 5, 61, 135),
+            onPressed: _addWallet,
+            child: const Icon(
+              Icons.add,
+              color: Colors.white,
+              size: 35,
+            ),
+          ),
+        ),
+        body: CustomScrollView(
+          slivers: [
+            SliverAppBar(
               backgroundColor: const Color.fromARGB(255, 19, 194, 110),
-            ),
-            floatingActionButton: AvatarGlow(
-              glowColor: const Color.fromARGB(255, 5, 61, 135),
-              child: FloatingActionButton(
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(50.0)),
-                backgroundColor: const Color.fromARGB(255, 5, 61, 135),
-                onPressed: _addWallet,
-                child: const Icon(
-                  Icons.add,
-                  color: Colors.white,
-                  size: 35,
+              expandedHeight: 200,
+              flexibleSpace: FlexibleSpaceBar(
+                  background: Container(
+                decoration: const BoxDecoration(
+                  gradient: LinearGradient(
+                      begin: Alignment.topLeft,
+                      end: Alignment.bottomRight,
+                      transform: GradientRotation(3.142 / 6),
+                      colors: [
+                      Color.fromARGB(255, 19, 194, 110),
+                      Color.fromARGB(255, 127, 0, 254),
+                      Color.fromARGB(255, 188, 247, 25),
+                      Color.fromARGB(255, 201, 249, 226),
+                  ])
+                  ),
+                child: PageView(
+                  children: [ 
+                    SelectedWallets(wallet: income),
+                    SelectedWallets(wallet: savings),
+                    SelectedWallets(wallet: debts),
+                    SelectedWallets(wallet: flexible),
+                    SelectedWallets(wallet: income),
+                  ]
                 ),
-              ),
+              )),
             ),
-            body: CustomScrollView(
-              slivers: [
-                SliverAppBar(
-                  backgroundColor: const Color.fromARGB(255, 19, 194, 110),
-                  expandedHeight: 200,
-                  flexibleSpace: FlexibleSpaceBar(
-                      background: Container(
-                    decoration: const BoxDecoration(
-                        gradient: LinearGradient(
-                            begin: Alignment.topLeft,
-                            end: Alignment.bottomRight,
-                            transform: GradientRotation(3.142 / 6),
-                            colors: [
-                          Color.fromARGB(255, 19, 194, 110),
-                          Color.fromARGB(255, 127, 0, 254),
-                          Color.fromARGB(255, 188, 247, 25),
-                          Color.fromARGB(255, 201, 249, 226),
-                        ])
+            SliverToBoxAdapter(
+              child: Container(
+                color: const Color.fromARGB(255, 241, 255, 248),
+                child: const Padding(
+                  padding: EdgeInsets.fromLTRB(20, 10.0, 20, 10),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "Your Wallets",
+                        style: TextStyle(
+                          fontSize: 30.0,
+                          color: Color.fromARGB(255, 48, 136, 6),
+                          fontWeight: FontWeight.w700,
                         ),
-                    child: PageView(
-                      children: [ 
-                        savingsCard(wallet: income),
-                        savingsCard(wallet: income),
-                        savingsCard(wallet: income),
-                        savingsCard(wallet: income),
-                        savingsCard(wallet: income),
-                        // savingsCard(),
-                        // savingsCard(),
-                      ]
-                    ),
-                  )),
-                ),
-                SliverToBoxAdapter(
-                  child: Container(
-                      color: const Color.fromARGB(255, 241, 255, 248),
-                      child: const Padding(
-                        padding: EdgeInsets.fromLTRB(20, 10.0, 20, 10),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              "Your Wallets",
-                              style: TextStyle(
-                                fontSize: 30.0,
-                                color: Color.fromARGB(255, 48, 136, 6),
-                                fontWeight: FontWeight.w700,
-                              ),
-                            ),
-                            SizedBox(
-                              height: 10,
-                            ),
-                            SizedBox(
-                              height: 2800,
-                              child: WalletsCard(),
-                            ),
-                          ],
-                        ),
-                      )),
-                )
-              ],
-            )));
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      SizedBox(
+                        height: 2800,
+                        child: WalletsCard(),
+                      ),
+                    ],
+                  ),
+                )),
+            )
+          ],
+        )
+      )
+    );
   }
 }
--- lib/cards/savings.dart
+++ lib/cards/savings.dart
@@ -1,72 +1,71 @@
 import "package:flutter/material.dart";
-import "package:app/utility/schema/methods.dart";
 import "package:app/models/schemas.dart";
-
 import "package:font_awesome_flutter/font_awesome_flutter.dart";
 
-class savingsCard extends StatelessWidget {
-  const savingsCard({required this.wallet, super.key});
+class SelectedWallets extends StatelessWidget {
+  const SelectedWallets({required this.wallet, super.key});
   final Wallet wallet;
 
   @override
   Widget build(BuildContext context) {
     return Padding(
-        padding: const EdgeInsets.fromLTRB(0, 20, 0, 10),
-        child: Column(
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                FloatingActionButton(
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(50)),
-                  elevation: 0,
-                  onPressed: () {},
-                  backgroundColor: const Color.fromARGB(255, 204, 241, 228),
-                  child: const Icon(
-                    Icons.add,
-                    size: 35,
-                    color: Color.fromARGB(255, 48, 136, 6),
-                  ),
+      padding: const EdgeInsets.fromLTRB(0, 20, 0, 10),
+      child: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              FloatingActionButton(
+                mini: true,
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(50)),
+                elevation: 0,
+                onPressed: () {},
+                backgroundColor: const Color.fromARGB(255, 204, 241, 228),
+                child: const Icon(
+                  Icons.add,
+                  size: 25,
+                  color: Color.fromARGB(255, 48, 136, 6),
                 ),
-                const Text("Wallets",
-                    style: TextStyle(
-                      fontSize: 25.0,
-                      color: Color.fromARGB(255, 255, 255, 255),
-                    )),
-                FloatingActionButton(
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(50)),
-                  elevation: 0,
-                  onPressed: () {},
-                  backgroundColor: const Color.fromARGB(255, 204, 241, 228),
-                  child: const FaIcon(FontAwesomeIcons.penToSquare,
-                      size: 24, color: Color.fromARGB(255, 48, 136, 6)),
-                ),
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text("${income.bal}",
-                    style: const TextStyle(
-                      fontSize: 35.0,
-                      fontWeight: FontWeight.w700,
-                      color: Color.fromARGB(255, 255, 255, 255),
-                    )),
-                // const Text(".00",
-                //     style: TextStyle(
-                //       fontSize: 35.0,
-                //       color: Color.fromARGB(207, 255, 255, 255),
-                //     )),
-              ],
-            ),
-            Text("${income.name}",
-                style: const TextStyle(
-                  fontSize: 23.0,
+              ),
+              const Text("Wallets",
+                style: TextStyle(
+                  fontSize: 25.0,
                   color: Color.fromARGB(255, 255, 255, 255),
                 )),
-          ],
-        ));
+              FloatingActionButton(
+                mini: true,
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(50)),
+                elevation: 0,
+                onPressed: () {},
+                backgroundColor: const Color.fromARGB(255, 204, 241, 228),
+                child: const FaIcon(FontAwesomeIcons.penToSquare,
+                    size: 20, color: Color.fromARGB(255, 48, 136, 6)),
+              ),
+            ],
+          ),
+          const SizedBox(
+            height: 12,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text("${wallet.bal}",
+                style: const TextStyle(
+                  fontSize: 35.0,
+                  fontWeight: FontWeight.w700,
+                  color: Color.fromARGB(255, 255, 255, 255),
+                )),
+            ],
+          ),
+          Text("${wallet.name}",
+            style: const TextStyle(
+              fontSize: 23.0,
+              color: Color.fromARGB(255, 255, 255, 255),
+            )),
+        ],
+      )
+    );
   }
 }
