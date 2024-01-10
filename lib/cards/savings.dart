b5a0d902a009fac1ab3db0b7f2e8e1793418e5e4
rasheedmhd <starletgh@gmail.com>
2024-01-10 11:16:29 +0000

    groundwork for editing wallets

--- lib/cards/savings.dart
+++ lib/cards/savings.dart
@@ -1,4 +1,3 @@
-import "package:avatar_glow/avatar_glow.dart";
 import "package:flutter/material.dart";
 import "package:app/models/schemas.dart";
 import "package:font_awesome_flutter/font_awesome_flutter.dart";
