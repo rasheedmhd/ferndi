1db61d8ecf5dcfd68869affd08082d95dc6351b8
rasheedmhd <starletgh@gmail.com>
2024-01-26 23:59:04 +0000

    groundwork for tab bars

--- lib/home/home.dart
+++ lib/home/home.dart
@@ -22,85 +22,94 @@ class HomeState extends ConsumerState<Home> {
     return MaterialApp(
         title: "Home",
         theme: ThemeData(fontFamily: 'Gilroy'),
-        home: Scaffold(
-            appBar: AppBar(
-              title: const Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
+        home: DefaultTabController(
+          length: 2,
+          child: Scaffold(
+              appBar: TabBarView(
                 children: [
-                  Row(
-                    children: [
-                      FaIcon(FontAwesomeIcons.chevronLeft,
-                          size: 20, color: Colors.white),
-                      SizedBox(
-                        width: 12,
-                      ),
-                      Text(
-                        "This Month",
-                        style: TextStyle(color: Colors.white),
-                      ),
-                      SizedBox(
-                        width: 12,
-                      ),
-                      FaIcon(FontAwesomeIcons.chevronRight,
-                          size: 20, color: Colors.white),
-                    ],
-                  ),
+                  hompage(),
                 ],
               ),
-              backgroundColor: const Color.fromARGB(255, 5, 61, 135),
-            ),
-            body: ListView(
-              padding: const EdgeInsets.all(15),
-              children: [
-                const BalanceCard(),
-                const SizedBox(
-                  height: 10,
-                ),
-                const AddSpendCard(),
-                const SizedBox(
-                  height: 20,
-                ),
-                GestureDetector(
-                  onTap: () => {
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => const SpendsPage()))
-                  },
-                  child: Row(
-                    children: [
-                      const Text(
-                        "Spend History",
-                        style: TextStyle(
-                          fontSize: 30.0,
-                          color: Color.fromARGB(255, 5, 61, 135),
-                          fontWeight: FontWeight.w700,
-                        ),
-                      ),
-                      const Spacer(),
-                      const FaIcon(FontAwesomeIcons.arrowRightLong,
-                          size: 23, color: Color.fromARGB(255, 151, 151, 151)),
-                      Text(
-                        " $ts ",
-                        style: const TextStyle(
-                          fontSize: 30.0,
-                          color: Color.fromARGB(255, 151, 151, 151),
-                          fontWeight: FontWeight.w700,
-                        ),
-                      ),
-                    ],
+              // appBar: 
+              // AppBar(
+              //   title: const Row(
+              //     mainAxisAlignment: MainAxisAlignment.spaceAround,
+              //     children: [
+              //       Row(
+              //         children: [
+              //           FaIcon(FontAwesomeIcons.chevronLeft,
+              //               size: 20, color: Colors.white),
+              //           SizedBox(
+              //             width: 12,
+              //           ),
+              //           Text(
+              //             "This Month",
+              //             style: TextStyle(color: Colors.white),
+              //           ),
+              //           SizedBox(
+              //             width: 12,
+              //           ),
+              //           FaIcon(FontAwesomeIcons.chevronRight,
+              //               size: 20, color: Colors.white),
+              //         ],
+              //       ),
+              //     ],
+              //   ),
+              //   backgroundColor: const Color.fromARGB(255, 5, 61, 135),
+              // ),
+              body: ListView(
+                padding: const EdgeInsets.all(15),
+                children: [
+                  const BalanceCard(),
+                  const SizedBox(
+                    height: 10,
                   ),
-                ),
-                const SizedBox(
-                  height: 10,
-                ),
-                const SizedBox(
-                  //padding: EdgeInsets.fromLTRB(15, 10, 0, 0),
-                  height: 2800,
-                  // height: double.infinity,
-                  child: Spends(),
-                ),
-              ],
-            )));
+                  const AddSpendCard(),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  GestureDetector(
+                    onTap: () => {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const SpendsPage()))
+                    },
+                    child: Row(
+                      children: [
+                        const Text(
+                          "Spend History",
+                          style: TextStyle(
+                            fontSize: 30.0,
+                            color: Color.fromARGB(255, 5, 61, 135),
+                            fontWeight: FontWeight.w700,
+                          ),
+                        ),
+                        const Spacer(),
+                        const FaIcon(FontAwesomeIcons.arrowRightLong,
+                            size: 23, color: Color.fromARGB(255, 151, 151, 151)),
+                        Text(
+                          " $ts ",
+                          style: const TextStyle(
+                            fontSize: 30.0,
+                            color: Color.fromARGB(255, 151, 151, 151),
+                            fontWeight: FontWeight.w700,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  const SizedBox(
+                    //padding: EdgeInsets.fromLTRB(15, 10, 0, 0),
+                    height: 2800,
+                    // height: double.infinity,
+                    child: Spends(),
+                  ),
+                ],
+              )),
+        ));
   }
 }
