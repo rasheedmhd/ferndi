cc264a89c553aee4d698dde5f5b37a2fa6798dbf
rasheedmhd <starletgh@gmail.com>
2024-02-05 16:06:19 +0000

    all categories

--- lib/Insights/categories.dart
+++ lib/Insights/categories.dart
@@ -1,6 +1,7 @@
-import "package:app/home/spends.dart";
+// import "package:app/home/spends.dart";
 // import "package:app/utility/schema/methods.dart";
-import "package:app/providers/spends_provider.dart";
+// import "package:app/providers/spends_provider.dart";
+import "package:app/Insights/category.dart";
 import "package:flutter/material.dart";
 import "package:font_awesome_flutter/font_awesome_flutter.dart";
 import "package:flutter_riverpod/flutter_riverpod.dart";
@@ -16,7 +17,7 @@ class SpendsPageState extends ConsumerState<Categories> {
   @override
   Widget build(BuildContext context) {
 
-  final spendsCount = ref.watch(spendsCountProvider);
+  // final spendsCount = ref.watch(spendsCountProvider);
   
     return MaterialApp(
         title: "Home",
@@ -37,77 +38,21 @@ class SpendsPageState extends ConsumerState<Categories> {
             ),
             body: ListView(
               padding: const EdgeInsets.all(15),
-              children: [
-                Container(
-                  padding: const EdgeInsets.all(3),
-                  decoration: BoxDecoration(
-                    border: Border.all(color: Colors.black),
-                    borderRadius: BorderRadius.circular(45.0),
-                  ),
-                  child: Row(
-                    children: [
-                      ClipRRect(
-                          borderRadius: BorderRadius.circular(50),
-                          child: Container(
-                              padding: const EdgeInsets.all(10),
-                              color: const Color.fromARGB(255, 5, 61, 135),
-                              child: const Text(
-                                " this week ",
-                                style: TextStyle(
-                                  color: Colors.white,
-                                  fontSize: 17,
-                                  fontWeight: FontWeight.w600,
-                                ),
-                              ))),
-                      const Spacer(),
-                      ClipRRect(
-                          borderRadius: BorderRadius.circular(50),
-                          child: Container(
-                              padding: const EdgeInsets.all(10),
-                              color: const Color.fromARGB(198, 255, 255, 255),
-                              child: const Text(
-                                " this month ",
-                                style: TextStyle(
-                                  color: Color.fromARGB(255, 5, 61, 135),
-                                  fontSize: 17,
-                                  fontWeight: FontWeight.w600,
-                                ),
-                              ))),
-                      const Spacer(),
-                      ClipRRect(
-                          borderRadius: BorderRadius.circular(50),
-                          child: Container(
-                              padding: const EdgeInsets.all(10),
-                              color: const Color.fromARGB(134, 255, 255, 255),
-                              child: const Text(
-                                " this year ",
-                                style: TextStyle(
-                                  color: Color.fromARGB(255, 5, 61, 135),
-                                  fontSize: 17,
-                                  fontWeight: FontWeight.w600,
-                                ),
-                              ))),
-                    ],
-                  ),
-                ),
-                const SizedBox(
-                  height: 15,
-                ),
-                Row(
+              children: const [
+              Row(
                   children: [
                     const Text(
                       "All Categories",
                       style: TextStyle(
                         fontSize: 30.0,
                         color: Color.fromARGB(255, 5, 61, 135),
-                        fontWeight: FontWeight.w700,
                       ),
                     ),
                     const Spacer(),
                     const FaIcon(FontAwesomeIcons.arrowRightLong,
                         size: 23, color: Color.fromARGB(255, 151, 151, 151)),
                     Text(
-                      " $spendsCount ",
+                      " 11 ",
                       style: const TextStyle(
                         fontSize: 30.0,
                         color: Color.fromARGB(255, 151, 151, 151),
@@ -121,7 +66,7 @@ class SpendsPageState extends ConsumerState<Categories> {
                 ),
                 const SizedBox(
                   height: 5600,
-                  child: Spends(),
+                  child: Categorys(),
                 ),
               ],
             )));
