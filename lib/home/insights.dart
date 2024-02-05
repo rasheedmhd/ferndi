dfd555b6b1aad662092a8907e9a8083c65343595
rasheedmhd <starletgh@gmail.com>
2024-02-05 15:26:38 +0000

    categories page

--- lib/home/insights.dart
+++ lib/home/insights.dart
@@ -1,8 +1,8 @@
 import "package:app/Insights/cards/delightful.dart";
 import 'package:app/Insights/cards/spendInfo.dart';
-import 'package:app/Insights/cards/spendsInfoCard.dart';
-import "package:app/home/spendHistory.dart";
+import 'package:app/Insights/Categories.dart';
 import "package:app/providers/spends_provider.dart";
+import 'package:app/Insights/cards/spendsInfoCard.dart';
 import "package:flutter/material.dart";
 import "package:flutter_riverpod/flutter_riverpod.dart";
 import "package:font_awesome_flutter/font_awesome_flutter.dart";
@@ -105,7 +105,7 @@ class HomeState extends ConsumerState<Insights> {
       GestureDetector(
         onTap: () => {
           Navigator.push(context,
-              MaterialPageRoute(builder: (context) => const SpendsPage()))
+              MaterialPageRoute(builder: (context) => const Categories()))
         },
         child: Container(
           padding: const EdgeInsets.all(20),
@@ -120,7 +120,7 @@ class HomeState extends ConsumerState<Insights> {
               Row(
                 children: [
                   const Text(
-                    "Categories",
+                    "All categories",
                     style: TextStyle(
                       fontSize: 30.0,
                       color: Color.fromARGB(255, 5, 61, 135),
@@ -140,52 +140,8 @@ class HomeState extends ConsumerState<Insights> {
                   ),             
                 ],
               ),
-              const Divider(
-                height: 0,
-                color: Color.fromARGB(255, 227, 226, 226),
-              ),
-              const Text("data"),              
-              const Text("data"),              
-              const Text("data"),              
-              const Text("see all"),  
-              Row(
-                children: [
-                  ClipRRect(
-                    borderRadius: BorderRadius.circular(50),
-                    child: Container(
-                      padding: const EdgeInsets.all(7),
-                      color: const Color.fromARGB(198, 235, 232, 255),
-                      child: const Text(
-                        "   see all   ",
-                        style: TextStyle(
-                          color: Color.fromARGB(199, 71, 34, 255),
-                          fontWeight: FontWeight.w600,
-                        ),
-                      )
-                    )
-                  ),
-                  const SizedBox(
-                    width: 20,
-                  ),                 
-                  ClipRRect(
-                    borderRadius: BorderRadius.circular(50),
-                    child: Container(
-                      padding: const EdgeInsets.all(7),
-                      color: const Color.fromARGB(198, 235, 232, 255),
-                      child: const Text(
-                        "   +   add category   ",
-                        style: TextStyle(
-                          color: Color.fromARGB(199, 71, 34, 255),
-                          fontWeight: FontWeight.w600,
-                        ),
-                      )
-                    )
-                  ),
-                ],
-              )             
             ],
           ),
-          
         ),
       ),
       const SizedBox(
