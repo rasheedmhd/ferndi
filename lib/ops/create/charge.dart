386a16e34d030f89bc4167817bf766ff29539b38
rasheedmhd <starletgh@gmail.com>
2024-03-22 21:47:48 +0000

    Charge Feature V1

--- lib/ops/create/charge.dart
+++ lib/ops/create/charge.dart
@@ -1,7 +1,12 @@
-import "package:app/providers/wallets_provider.dart";
+import "package:app/providers/spends_provider.dart";
+import 'package:app/providers/wallets_provider.dart' as P;
+import "package:app/utility/schema/methods.dart";
+
+import 'package:app/models/schemas.dart';
 import 'package:app/models/schemas.dart' as my;
 import "package:flutter/material.dart";
 import "package:flutter_riverpod/flutter_riverpod.dart";
+import "package:realm/realm.dart";
 
 class ChargeCard extends ConsumerStatefulWidget {
   final my.Subscription subscription;
@@ -14,10 +19,13 @@ class ChargeCard extends ConsumerStatefulWidget {
 class ChargeCardState extends ConsumerState<ChargeCard> {
   @override
   Widget build(BuildContext context) {
-    final sub = widget.subscription;
+    final subscriptionToCharge = widget.subscription;
+    Category subscriptionToChargeCategory = categories.isEmpty
+      ? Category(ObjectId(), "Subscriptions", "💞", "", DateTime.now())
+      : categories.first;
 
     return SizedBox(
-      height: 300,
+      height: 320,
       child: Padding(
         padding: const EdgeInsets.all(20),
         child: Container(
@@ -42,7 +50,7 @@ class ChargeCardState extends ConsumerState<ChargeCard> {
                     height: 10,
                   ),                  
                   const Text("Charge is how you pay your Subscriptions."),
-                  Text("If you Charge, GHS ${sub.getAmount} will be deducted from ${sub.wallet!.name} to pay for ${sub.name} and Recorded as a Spend"),
+                  Text("If you Charge, GHS ${subscriptionToCharge.getAmount} will be deducted from ${subscriptionToCharge.wallet!.name} to pay for ${subscriptionToCharge.name} and Recorded as a Spend"),
                   const SizedBox(
                     height: 10,
                   ),
@@ -50,6 +58,91 @@ class ChargeCardState extends ConsumerState<ChargeCard> {
 
                   const SizedBox(
                     height: 10,
+                  ),
+                   FloatingActionButton.extended(
+                    elevation: 1,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(50.0),
+                    ),
+                    label: const Text(
+                      "          Charge         ",
+                      style: TextStyle(
+                        fontWeight: FontWeight.w700,
+                      ),
+                    ),
+                    foregroundColor: const Color.fromARGB(255, 255, 255, 255),
+                    backgroundColor: const Color.fromARGB(255, 5, 61, 135),
+                    onPressed: () {
+                      if (subscriptionToCharge.amount >
+                          subscriptionToCharge.wallet!.balance) {
+                        ScaffoldMessenger.of(context).clearSnackBars();
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          const SnackBar(
+                            backgroundColor: Color.fromARGB(255, 255, 231, 241),
+                            content: Column(
+                              children: [
+                                Text(
+                                  "You don't have enough money in the Wallet",
+                                  style: TextStyle(
+                                    color: Color.fromARGB(255, 163, 9, 71),
+                                  ),
+                                ),
+                                Text(
+                                  "Go to Accounts and top up first.",
+                                  style: TextStyle(
+                                    color: Color.fromARGB(255, 163, 9, 71),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        );
+                        return;
+                      } else {
+                        ref.read(spendsNotifier.notifier).recordSpend(
+                              Spend(
+                                ObjectId(),
+                                "Paid for ${subscriptionToCharge.name}",
+                                "Paid for ${subscriptionToCharge.name} which happens every ${subscriptionToCharge.createdAt}  ",
+                                subscriptionToCharge.amount,
+                                category: subscriptionToChargeCategory,
+                                wallet: subscriptionToCharge.wallet,
+                                DateTime.now(),
+                              ),
+                            );
+                        ref.read(P.walletsNotifier.notifier).updateWallet(
+                              Wallet(subscriptionToCharge.wallet!.id, subscriptionToCharge.wallet!.name,
+                                  subscriptionToCharge.amount, DateTime.now()),
+                            );
+                        ScaffoldMessenger.of(context).clearSnackBars();
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          const SnackBar(
+                            backgroundColor: Color.fromARGB(255, 231, 255, 245),
+                            content: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Row(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    Text(
+                                      "Spend successfully recorded. ",
+                                      style: TextStyle(
+                                        color: Color.fromARGB(255, 9, 163, 99),
+                                      ),
+                                    ),
+                                    Icon(
+                                      Icons.sentiment_very_satisfied,
+                                      color: Color.fromARGB(255, 9, 163, 9),
+                                    )
+                                  ],
+                                ),
+                              ],
+                            ),
+                          ),
+                        );
+                      }
+                      Navigator.of(context).pop();
+                    },
                   ),
                   // ref.watch(subscriptionsNotifier.notifier).addSubscription(
                   //       my.Subscription(
